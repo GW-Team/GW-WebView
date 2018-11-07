50dd8c3cd262ed356d251e7ffd7e3cb1b1ec560b
Raiven Kao <omegaatt36@gmail.com>
2018-11-07 20:19:36 +0800

    change device

---
+++ src/pages/home/home.ts
@@ -0,0 +1,54 @@
+import { Component } from '@angular/core';
+import { NavController, Platform } from 'ionic-angular';
+import { InAppBrowser, InAppBrowserOptions ,InAppBrowserEvent } from '@ionic-native/in-app-browser';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+  //template: "<p>....loading</p>"
+})
+export class HomePage {
+
+  url: string;
+
+  constructor(public navCtrl: NavController,private inAppBrowser : InAppBrowser,platform: Platform) {  
+    platform.ready().then(() =>{
+      
+      const options: InAppBrowserOptions = {
+        zoom: 'no',
+        location: 'no',
+        hardwareback: 'yes',
+        hideurlbar: 'yes',
+        hidenavigationbuttons: 'yes',
+        enableViewportScale: 'no',
+        fullscreen: 'yes'
+      }
+      
+      let browser = this.inAppBrowser.create('https://gowhere-0107402.appspot.com/users/sign_in','_self',options)
+
+      browser.on('exit').subscribe((event: InAppBrowserEvent) => {
+        platform.exitApp();
+      });
+
+      browser.show();
+      
+    });
+
+    //this.openWebpage("https://ionicframework.com/docs/native/in-app-browser/")
+  }
+
+  
+  /*
+  openWebpage(url: string){
+
+    const options: InAppBrowserOptions = {
+      zoom: 'no',
+      location: 'no'
+    }
+
+    const browser = this.inAppBrowser.create(url, '_self',);
+  }
+  */
+
+
+}
